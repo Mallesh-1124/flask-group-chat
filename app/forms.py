d28e7adb7619202c0e17096ad0231c91f55fda66
Mallesh Kolipaka <kmallesh.1124@gmail.com>
2025-11-08 10:45:18 +0530

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from app.models import User
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
