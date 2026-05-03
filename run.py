7f953f2d73588fe72770c0ff0f0510611978c2cf
Mallesh-1124 <kmallesh.1124@gmail.com>
2026-05-03 08:46:24 +0530

    fix: add allow_unsafe_werkzeug=True to fix Flask-SocketIO 5.x startup crash

--- run.py
+++ run.py
@@ -6,4 +6,10 @@ from app import app, socketio
 
 if __name__ == '__main__':
     debug_mode = os.environ.get('FLASK_DEBUG', 'false').lower() == 'true'
-    socketio.run(app, debug=debug_mode, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    socketio.run(
+        app,
+        debug=debug_mode,
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000)),
+        allow_unsafe_werkzeug=True   # required for Flask-SocketIO 5.x dev server
+    )
